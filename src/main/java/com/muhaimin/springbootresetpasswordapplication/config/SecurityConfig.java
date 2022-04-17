c1b5290ce3ecd2ae7fbc7119b0d312b9ac556a3c
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-17 11:51:03 +0700

    #6 - Add configure in SecurityConfig

--- src/main/java/com/muhaimin/springbootresetpasswordapplication/config/SecurityConfig.java
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.muhaimin.springbootresetpasswordapplication.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -9,5 +10,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
 
 }
