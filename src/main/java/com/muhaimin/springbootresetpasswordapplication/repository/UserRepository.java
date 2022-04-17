271381902d55c6897250c63e2cc70c2b151d1e26
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-17 12:53:09 +0700

    #21 - Add UserRepository

---
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.muhaimin.springbootresetpasswordapplication.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
