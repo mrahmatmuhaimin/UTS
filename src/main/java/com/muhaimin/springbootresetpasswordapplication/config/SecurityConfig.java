7b80a22c848256e10c5e632f664ceff9d030d5cd
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-17 11:53:04 +0700

    #7 - Add passwordEncoder

--- src/main/java/com/muhaimin/springbootresetpasswordapplication/config/SecurityConfig.java
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.muhaimin.springbootresetpasswordapplication.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,4 +21,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(
+                        "/signup**",
+                        "/forgot-password**",
+                        "/reset-password**").permitAll()
+                .antMatchers(
+                        "/js/**",
+                        "/css/**",
+                        "/img/**",
+                        "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
