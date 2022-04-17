ddd845d58ba91c2219196169603a976207ff84a1
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-17 13:51:52 +0700

    #42 - Add SignupController

---
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/controller/SignupController.java
@@ -0,0 +1,20 @@
+package com.muhaimin.springbootresetpasswordapplication.controller;
+
+import com.muhaimin.springbootresetpasswordapplication.service.framework.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+    private final UserService userService;
+    private final MessageSource messageSource;
+
+    @Autowired
+    public SignupController(UserService userService, MessageSource messageSource) {
+        this.userService = userService;
+        this.messageSource = messageSource;
+    }
+}
