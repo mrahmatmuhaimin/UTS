be43a7f124f080736f9e90ac9e64c53d6df270a8
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-17 11:14:28 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/SpringBootForgotPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.muhaimin.springbootresetpasswordapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForgotPasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootForgotPasswordApplication.class, args);
+    }
+
+}
