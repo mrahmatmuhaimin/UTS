7b606144738db43baf952fe8b758ba40be2a0219
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-17 14:03:58 +0700

    #49 - Bug Fix Circular Dependency

--- src/main/java/com/muhaimin/springbootresetpasswordapplication/SpringBootForgotPasswordApplication.java
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/SpringBootForgotPasswordApplication.java
@@ -2,6 +2,8 @@ package com.muhaimin.springbootresetpasswordapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootForgotPasswordApplication {
@@ -10,4 +12,8 @@ public class SpringBootForgotPasswordApplication {
         SpringApplication.run(SpringBootForgotPasswordApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
