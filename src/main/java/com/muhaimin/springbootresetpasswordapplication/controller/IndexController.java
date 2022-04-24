cb0b2990f43fb6fb331de4f2c5c6692ffa6ee209
Muhaimin <muhaiminrahmat@gmail.com>
2022-04-24 15:12:24 +0700

    #65 - Add Student Entity

--- src/main/java/com/muhaimin/springbootresetpasswordapplication/controller/IndexController.java
+++ src/main/java/com/muhaimin/springbootresetpasswordapplication/controller/IndexController.java
@@ -3,15 +3,7 @@ package com.muhaimin.springbootresetpasswordapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : spring-boot-reset-password
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 12/04/22
- * Time: 09.51
- */
+
 @Controller
 public class IndexController {
     @GetMapping("/")
